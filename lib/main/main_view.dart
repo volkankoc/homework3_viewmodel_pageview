79609dfd195332fcfcec9c1a07af30a87eda030e
Volkan KOÇ <volkankoc53@gmail.com>
2023-05-29 23:20:14 +0300

    statelesswidget dönüşümü yapıldı

--- lib/main/main_view.dart
+++ lib/main/main_view.dart
@@ -4,45 +4,33 @@ import 'package:homework3_viewmodel_pageview/page/page_view_model.dart';
 import 'package:homework3_viewmodel_pageview/provider/page_provider.dart';
 import 'package:provider/provider.dart';
 
-class MainView extends StatefulWidget {
-  const MainView({super.key});
+class MainView extends StatelessWidget {
+  MainView({super.key});
 
-  @override
-  State<MainView> createState() => _MainViewState();
-}
-
-class _MainViewState extends State<MainView> {
   final pageList = PageViewModel();
-  int _currentIndex = 0;
+
   final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: PageView(
-          onPageChanged: _onPageChanged,
+          onPageChanged: (value) {
+            context.read<PageProvider>().changePage(value);
+            print(context.read<PageProvider>().getCurrentIndex().toString());
+          },
           controller: _pageController,
           children: pageList.pageList.map((e) => e.page).toList(),
         ),
-        bottomNavigationBar: _navBar());
-  }
-
-  void _onPageChanged(value) {
-    context.read<PageProvider>().changePage(value);
-    print(context.read<PageProvider>().getCurrentIndex().toString());
-   
-  }
-
-  BottomNavigationBar _navBar() {
-    return BottomNavigationBar(
-        currentIndex: context.watch<PageProvider>().getCurrentIndex(),
-        onTap: (value) {
-          context.read<PageProvider>().changePage(value);
-          _pageController.jumpToPage(value);
-        
-        },
-        items: NavbarViewModel.navbarList
-            .map((e) =>
-                BottomNavigationBarItem(icon: Icon(e.iconData), label: e.label))
-            .toList());
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: context.watch<PageProvider>().getCurrentIndex(),
+            onTap: (value) {
+              context.read<PageProvider>().changePage(value);
+              _pageController.jumpToPage(value);
+            },
+            items: NavbarViewModel.navbarList
+                .map((e) => BottomNavigationBarItem(
+                    icon: Icon(e.iconData), label: e.label))
+                .toList()));
   }
 }
